566f4ba8e4eca7ade9fefcb169f3faffc61ff849
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-10-14 14:14:47 -0700

    added real sieve

--- HW2-EX05/src/main.py
+++ HW2-EX05/src/main.py
@@ -2,7 +2,8 @@
 def main():
     the_input = input("Enter a number\n>>")
     the_primes = find_primes_less_than(int(the_input))
-    print(the_primes)
+    the_sieve = sieve(int(the_input))
+    print(the_sieve)
 
 
 def find_primes_less_than(n):
@@ -13,6 +14,14 @@ def find_primes_less_than(n):
                 the_range.remove(j)
     return the_range
 
+def sieve(n):
+    the_range = list(range(n))  # initialize the list of numbers ranging from 0 to n-1
+    for i in the_range:  # for every number from 0 to n-1
+        for j in range(i*i, n, i): # from i^2 to n, in increments of i
+            the_range[0] = 0  # set to 0
+
+    the_range = filter(lambda a: a != 0, the_range)  # filter out all 0s from the final list
+    return the_range
 
 if __name__ == "__main__":
     main()
